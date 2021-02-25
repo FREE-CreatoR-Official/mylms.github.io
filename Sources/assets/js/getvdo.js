41d1bf157e8aa278e711ad0184cde26ade47f87e
Free-CreatoR-Official <>
2021-02-25 08:28:46 +0530

    add main files.

---
+++ Sources/assets/js/getvdo.js
@@ -0,0 +1,5 @@
+function getLink() {
+            var href = document.getElementById("txtlink");
+            var video = document.getElementById("vdo");
+            vdo.src = txtbox.value;
+        }
